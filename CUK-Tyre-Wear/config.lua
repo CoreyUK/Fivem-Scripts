4864a88e7b1a7926d4e7cae0a42d0cbf07055a9e
Keyboard Sped <93077330+CoreyUK@users.noreply.github.com>
2025-10-19 12:03:24 +0100

    Update tire degradation rate in config.lua
    Adjusted the degradation rate for tire wear to reflect a more realistic in-game driving duration.

--- CUK-Tyre-Wear/config.lua
+++ CUK-Tyre-Wear/config.lua
@@ -1,8 +1,9 @@
 TyreWearConfig = {
     MAX_TYRE_HEALTH = 100.0,            -- Maximum tire durability
-    DEGRADATION_RATE = 0.0005,          -- Degradation per km/h
+    DEGRADATION_RATE = 0.000009,          -- Degradation per km/h  Roughly 30 Hours driving in-game
     ENABLE_DIFFERENTIAL_WEAR = true,    -- Higher wear on driven wheels
     ENABLE_OFFROAD_WEAR = true,         -- Increased wear on off-road surfaces
     OFFROAD_WEAR_MULTIPLIER = 2.5,      -- Off-road wear multiplier
     ENABLE_TYRE_REPAIR = true           -- Enable /fixmytyres command
 }
+
